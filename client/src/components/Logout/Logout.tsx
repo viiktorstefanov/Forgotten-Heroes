fc1958b62eedb32fdae46e0f24e867ee64cb54ff
Viktor Stefanov <victorystefanov@gmail.com>
2024-08-03 14:16:07 +0300

    logout  component

--- client/src/components/Logout/Logout.tsx
+++ client/src/components/Logout/Logout.tsx
@@ -1,10 +1,21 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
+import { logout } from "../../state/auth/authThunks";
+import { RootState, AppDispatch  } from "../../state/store";
+import { Navigate } from "react-router-dom";
 
 const Logout: React.FC = () => {
+  const user = useSelector((state: RootState) => state.auth.user);
+
+  const dispatch = useDispatch<AppDispatch>();
+
+  useEffect(() => {
+    if (user) {
+      dispatch(logout(user));
+    }
+  }, [user, dispatch]);
  
-  return (
-    <></>
-  );
+  return <Navigate to='/'/>;
 };
 
 export default Logout;
