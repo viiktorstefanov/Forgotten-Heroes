8698326478d7b8fdc7ee4a5bb95357bfef36c162
Viktor Stefanov <victorystefanov@gmail.com>
2024-08-11 14:33:20 +0300

    update title for user model schena

--- server/services/userService.js
+++ server/services/userService.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 
-const User = require("../models/user");
+const User = require("../models/User");
 
 const bcrypt = require('bcryptjs');
 const jwt = require('jsonwebtoken');
